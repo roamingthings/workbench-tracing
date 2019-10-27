a887e5f96a88c1d4699bb02ba4254b8866732bdc
Alexander Sparkowsky <info@roamingthings.de>
2019-10-27 09:24:12 +0100

    [novel-ai] Extract mocking of tracing

---
+++ novel-ai-service/src/test/kotlin/de/roamingthings/tracing/novelai/test/Tracing.kt
@@ -0,0 +1,24 @@
+package de.roamingthings.tracing.novelai.test
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import io.opentracing.Scope
+import io.opentracing.Span
+import io.opentracing.Tracer
+import org.mockito.Mockito
+
+fun mockTracingLenientUsingMock(tracer: Tracer) {
+    val spanMock = mock<Span>(lenient = true)
+    val scopeMock = mock<Scope>(lenient = true)
+    val builderMock = mock<Tracer.SpanBuilder>(lenient = true) {
+        //            on { start() } doReturn spanMock
+        on { startActive(any()) } doReturn scopeMock
+    }
+    Mockito.lenient().doReturn(builderMock)
+            .`when`(tracer).buildSpan(any())
+    Mockito.lenient().doReturn(spanMock)
+            .`when`(tracer).activeSpan()
+    Mockito.lenient().doReturn(scopeMock)
+            .`when`(tracer).activateSpan(any())
+}
