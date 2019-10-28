e4eea28ef4979a3840fd3ac26ab7f82f2662846b
Alexander Sparkowsky <info@roamingthings.de>
2019-10-28 19:46:34 +0100

    [author] Add health checks for downstream services (#16)

---
+++ author-service/src/main/kotlin/de/roamingthings/tracing/author/health/DownstreamHealthIndicator.kt
@@ -0,0 +1,31 @@
+package de.roamingthings.tracing.author.health
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+
+
+open class DownstreamHealthIndicator(
+        private val restTemplate: RestTemplate,
+        private val downstreamServiceName: String,
+        private val downStreamUrl: String = "/actuator") : HealthIndicator {
+
+    override fun health(): Health {
+        try {
+            val response = restTemplate.getForObject("$downStreamUrl/health", JsonNode::class.java)
+            if (response != null && response.get("status").asText().equals("UP", ignoreCase = false)) {
+                return Health.up().build()
+            }
+        } catch (ex: Exception) {
+            return Health.down(ex).build()
+        }
+
+        return Health.down().withDetail("service", downstreamServiceName).build()
+    }
+}
+
+@Component
+class TextLibraryServiceHealthIndicator(textLibraryServiceRestTemplate: RestTemplate) :
+        DownstreamHealthIndicator(textLibraryServiceRestTemplate, "text-library-service")
